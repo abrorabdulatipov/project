4a7f159730d1551d356d9256c496fe3cc8bd8165
abrorabdulatipov <abrorabdulatipov2028@gmail.com>
2022-08-15 08:53:51 +0500

    12.08.2022

---
+++ tkinter/tkinter darslari/oyna yarat.py
@@ -0,0 +1,15 @@
+# from tkinter import *
+# root = Tk()
+# root.title("1-oyna")
+# root.geometry("300x200")
+# def oynanioch():
+#     oyna2 = Toplevel(root)
+#     oyna2.title("2-oyna")
+#     oyna2.geometry("300x200")
+#     def oynayop():
+#         oyna2.destroy()
+#     Button(root, text='JONINGIZGA TIGIB KETGAN OYNAGINGIZNI YOPING', command=oynayop).pack()
+#     oyna2.mainloop()
+# t = Button(root, text='BUYURTMA BERGAN OYNAGINGIZ OCH',bg='red',fg='white',command=oynanioch).pack(fill=BOTH,expand=True)
+#
+# root.mainloop()
